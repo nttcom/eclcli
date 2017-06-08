ceab13394ce7fd2e12c9e7c9feac2b5c674d3040
xinni-ge <xinni.ge@ntt.com>
2017-06-08 15:06:01 +0900

    Add qos list filters.

--- eclcli/network/v2/qos_option.py
+++ eclcli/network/v2/qos_option.py
@@ -7,14 +7,45 @@ class ListQosOption(command.Lister):
     def get_parser(self, prog_name):
         parser = super(ListQosOption, self).get_parser(prog_name)
         parser.add_argument(
+            '--name',
+            metavar="name",
+            help="filter by name")
+        parser.add_argument(
+            '--qos-type',
+            metavar="qos_type",
+            help="filter by qos_type: besteffort, guaranteed")
+        parser.add_argument(
             '--service-type',
             metavar="service_type",
-            help="filter by service_type: internet, vpn, interdc")
+            help="filter by service_type: aws, interdc, internet, vpn")
+        parser.add_argument(
+            '--aws-service-id',
+            metavar="aws_service_id",
+            help="filter by aws service id")
+        parser.add_argument(
+            '--interdc-service-id',
+            metavar="interdc_service_id",
+            help="filter by interdc service id")
+        parser.add_argument(
+            '--internet-service-id',
+            metavar="internet_service_id",
+            help="filter by internet service id")
+        parser.add_argument(
+            '--vpn-service-id',
+            metavar="vpn_service_id",
+            help="filter by vpn service id")
+        parser.add_argument(
+            '--status',
+            metavar="status",
+            help="filter by status")
+        parser.add_argument(
+            '--bandwidth',
+            metavar="bandwidth",
+            help="filter by bandwidth")
         return parser
 
     def take_action(self, parsed_args):
         network_client = self.app.client_manager.network
-        service_type = parsed_args.service_type
         columns = (
             'id',
             'name',
@@ -30,8 +61,24 @@ class ListQosOption(command.Lister):
             'Status',
         )
         params = dict()
-        if service_type:
-            params.update({"service_type": service_type})
+        if parsed_args.name:
+            params.update({"name": parsed_args.name})
+        if parsed_args.service_type:
+            params.update({"service_type": parsed_args.service_type})
+        if parsed_args.qos_type:
+            params.update({"qos_type": parsed_args.qos_type})
+        if parsed_args.aws_service_id:
+            params.update({"aws_service_id": parsed_args.aws_service_id})
+        if parsed_args.interdc_service_id:
+            params.update({"interdc_service_id": parsed_args.interdc_service_id})
+        if parsed_args.internet_service_id:
+            params.update({"internet_service_id": parsed_args.internet_service_id})
+        if parsed_args.vpn_service_id:
+            params.update({"vpn_service_id": parsed_args.vpn_service_id})
+        if parsed_args.status:
+            params.update({"status": parsed_args.status})
+        if parsed_args.bandwidth:
+            params.update({"bandwidth": parsed_args.bandwidth})
 
         data = [to_obj.QosOption(inetsv)
                 for inetsv in network_client.list_qos_options(
