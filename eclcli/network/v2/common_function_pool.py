aaa12a747f7499ed19977b7c5bf193d5701fbd08
xinni-ge <xinni.ge@ntt.com>
2017-06-12 14:19:03 +0900

    Add common function pool filters.

--- eclcli/network/v2/common_function_pool.py
+++ eclcli/network/v2/common_function_pool.py
@@ -6,6 +6,14 @@ from ..networkclient.common import utils as to_obj
 class ListCommonFunctionPool(command.Lister):
     def get_parser(self, prog_name):
         parser = super(ListCommonFunctionPool, self).get_parser(prog_name)
+        parser.add_argument(
+            '--id',
+            metavar="id",
+            help="filter by id")
+        parser.add_argument(
+            '--name',
+            metavar="name",
+            help="filter by name")
         return parser
 
     def take_action(self, parsed_args):
@@ -21,9 +29,15 @@ class ListCommonFunctionPool(command.Lister):
             'Name',
             'Description',
         )
+        search_opts = dict()
+        if parsed_args.id:
+            search_opts.update({"id": parsed_args.id})
+        if parsed_args.name:
+            search_opts.update({"name": parsed_args.name})
 
         data = [to_obj.CommonFunctionPool(cfp)
-            for cfp in network_client.list_common_function_pools().get('common_function_pools')]
+                for cfp in network_client.list_common_function_pools(
+                    **search_opts).get('common_function_pools')]
 
         return (column_headers,
                 (utils.get_item_properties(
@@ -36,7 +50,7 @@ class ShowCommonFunctionPool(command.ShowOne):
         parser = super(ShowCommonFunctionPool, self).get_parser(prog_name)
         parser.add_argument(
             'common_function_pool_id',
-            metavar="COMMON_FUNCTION_ID",
+            metavar="COMMON_FUNCTION_POOL_ID",
             help="ID of Common Function Pool to show."
         )
         return parser
