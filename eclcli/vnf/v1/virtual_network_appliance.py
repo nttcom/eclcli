97e510bdabb4a122449aa63d4c713ffb75687012
Keiichi Hikita <keiichi.hikita@ntt.com>
2017-12-05 18:14:56 +0900

    add plan management cli.

--- eclcli/vnf/v1/virtual_network_appliance.py
+++ eclcli/vnf/v1/virtual_network_appliance.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import json
 import copy
 import six
 from eclcli.common import command
@@ -59,9 +60,11 @@ class ShowVirtualNetworkAppliance(command.ShowOne):
         ]
         row_headers = rows
 
-        # TODO format interface dictionary by using kind of formetter function
         data = client.get_virtual_network_appliance(
             parsed_args.virtual_network_appliance_id)
+
+        _set_interfaces_for_display(data)
+
         return (row_headers, (utils.get_item_properties(data, rows)))
 
 
@@ -129,7 +132,7 @@ class CreateVirtualNetworkAppliance(command.ShowOne):
         row_headers = rows
 
         if len(parsed_args.interface) == 0:
-            msg = _("You can specify at least one interface.")
+            msg = _("You must specify at least one interface.")
             raise exceptions.CommandError(msg)
 
         if len(parsed_args.interface) > 1:
@@ -149,7 +152,24 @@ class CreateVirtualNetworkAppliance(command.ShowOne):
 
             interfaces.append(if_info)
 
-        interfaces_arg = _define_interface(parsed_args.interface)
+        interface_object = {}
+        if_num = 1
+        for interface in interfaces:
+
+            if_key = 'interface_' + str(if_num)
+            tmp = {
+                if_key: {
+                    'network_id': interface['net-id'],
+                    'fixed_ips': [
+                        {'ip_address': interface['fixed-ip']}
+                    ]
+                }
+            }
+            if interface['name']:
+                tmp[if_key].update({'name': interface['name']})
+
+            interface_object.update(tmp)
+            if_num += 1
 
         plan_id = \
             parsed_args.virtual_network_appliance_plan_id
@@ -161,7 +181,7 @@ class CreateVirtualNetworkAppliance(command.ShowOne):
 
         data = client.create_virtual_network_appliance(
             virtual_network_appliance_plan_id=plan_id,
-            interfaces=interfaces_arg,
+            interfaces=interface_object,
             name=name,
             description=description,
             default_gateway=default_gateway,
@@ -169,6 +189,133 @@ class CreateVirtualNetworkAppliance(command.ShowOne):
         )
 
         return (row_headers, utils.get_item_properties(data, rows))
+
+
+class DeleteVirtualNetworkAppliance(command.Command):
+
+    def get_parser(self, prog_name):
+        parser = super(DeleteVirtualNetworkAppliance, self).\
+            get_parser(prog_name)
+        parser.add_argument(
+            "virtual_network_appliance_id",
+            nargs="+",
+            help="IDs of virtual network appliance to be deleted",
+            metavar='<virtual-network-appliance-id>'
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        client = self.app.eclsdk.conn.virtual_network_appliance
+        for virtual_network_appliance_id in \
+                parsed_args.virtual_network_appliance_id:
+            client.delete_virtual_network_appliance(
+                virtual_network_appliance_id)
+
+
+class UpdateVirtualNetworkAppliance(command.ShowOne):
+    """
+    T.B.D ...
+    Now I'm asking spec about updating of VNF ?
+    """
+
+    def get_parser(self, prog_name):
+        parser = super(UpdateVirtualNetworkAppliance, self).\
+            get_parser(prog_name)
+        parser.add_argument(
+            'virtual_network_appliance_id',
+            metavar='<virtual-network-appliance-id>',
+            help='ID of virtual network appliance')
+        parser.add_argument(
+            "--name",
+            help="Name of virtual network appliance",
+            metavar='<string>'
+        )
+        parser.add_argument(
+            "--description",
+            help="Description of virtual network appliance",
+            metavar='<string>'
+        )
+
+        parser.add_argument(
+            '--interface',
+            metavar="<net-id=net-uuid,fixed-ip=ip-addr,name=interface-name>,",
+            action='append',
+            default=[],
+            help=_("Specify interface parameter for VNF. "
+                   "You can specify only one interface in creation of "
+                   "virtual network appliance. "
+                   "net-id: attach interface to network with this UUID, "
+                   "fixed-ip: IPv4 fixed address for NIC, "
+                   "name: Name of Interface (optional)."),
+        )
+
+        return parser
+
+    def take_action(self, parsed_args):
+        client = self.app.eclsdk.conn.virtual_network_appliance
+
+        rows = [
+            'ID',
+            'Name',
+            'Appliance Type',
+            'OS Monitoring Status',
+            'OS Login Status',
+            'VM Status',
+            'Interfaces'
+        ]
+        row_headers = rows
+
+        body = {}
+        if parsed_args.name:
+            body["name"] = parsed_args.name
+        if parsed_args.description:
+            body["description"] = parsed_args.description
+
+
+        if parsed_args.interfaces:
+
+            interfaces = []
+            for if_str in parsed_args.interface:
+                if_info = {"net-id": "", "fixed-ip": "",
+                           "name": ""}
+                if_info.update(dict(kv_str.split("=", 1)
+                               for kv_str in if_str.split(",")))
+                if not bool(if_info["net-id"]) or not bool(if_info["fixed-ip"]):
+                    msg = _("You must specify network uuid and ip address both")
+                    raise exceptions.CommandError(msg)
+
+                interfaces.append(if_info)
+
+            interface_object = {}
+            if_num = 1
+            for interface in interfaces:
+
+                if_key = 'interface_' + str(if_num)
+                tmp = {
+                    if_key: {
+                        'network_id': interface['net-id'],
+                        'fixed_ips': [
+                            {'ip_address': interface['fixed-ip']}
+                        ]
+                    }
+                }
+                if interface['name']:
+                    tmp[if_key].update({'name': interface['name']})
+
+                interface_object.update(tmp)
+                if_num += 1
+
+            body["interfaces"] = None
+
+        data = client.update_snapshot(parsed_args.snapshot_id, **body)
+
+        return (row_headers, utils.get_item_properties(data, rows))
+
+
+def _set_interfaces_for_display(data):
+    ifs = data.interfaces
+    interfaces_json = json.dumps(ifs, indent=2)
+    setattr(data, 'interfaces', interfaces_json)
 
 
 def _define_interface(arg_interface):
@@ -204,90 +351,3 @@ def _define_interface(arg_interface):
         if_num += 1
 
     return interface_object
-
-
-class DeleteVirtualNetworkAppliance(command.Command):
-
-    def get_parser(self, prog_name):
-        parser = super(DeleteVirtualNetworkAppliance, self).\
-            get_parser(prog_name)
-        parser.add_argument(
-            "virtual_network_appliance_id",
-            nargs="+",
-            help="IDs of virtual network appliance to be deleted",
-            metavar='<virtual-network-appliance-id>'
-        )
-        return parser
-
-    def take_action(self, parsed_args):
-        client = self.app.eclsdk.conn.virtual_network_appliance
-        for virtual_network_appliance_id in \
-                parsed_args.virtual_network_appliance_id:
-            client.delete_virtual_network_appliance(
-                virtual_network_appliance_id)
-
-
-# class UpdateSnapshot(command.ShowOne):
-#
-#     def get_parser(self, prog_name):
-#         parser = super(UpdateSnapshot, self).get_parser(prog_name)
-#         parser.add_argument(
-#             'snapshot_id',
-#             metavar='<string>',
-#             help='ID of snapshot')
-#         parser.add_argument(
-#             "--name",
-#             help="Name of snapshot",
-#             metavar='<string>'
-#         )
-#         parser.add_argument(
-#             "--description",
-#             help="Description of snapshot",
-#             metavar='<string>'
-#         )
-#         return parser
-#
-#     def take_action(self, parsed_args):
-#         client = self.app.eclsdk.conn.storage
-#
-#         rows = [
-#             'ID',
-#             'Name',
-#             'Status',
-#             'Description',
-#             'Usage',
-#             'Volume ID',
-#             'Snapshot Type ID',
-#             'Created At',
-#             'Updated At',
-#             'Deleted At',
-#             'Error Message',
-#             'Delete Reason',
-#         ]
-#         row_headers = rows
-#
-#         body = {}
-#         if parsed_args.name:
-#             body["name"] = parsed_args.name
-#         if parsed_args.description:
-#             body["description"] = parsed_args.description
-#         data = client.update_snapshot(parsed_args.snapshot_id, **body)
-#
-#         return (row_headers, utils.get_item_properties(data, rows))
-#
-#
-#
-#
-# class RestoreSnapshot(command.Command):
-#
-#     def get_parser(self, prog_name):
-#         parser = super(RestoreSnapshot, self).get_parser(prog_name)
-#         parser.add_argument(
-#             'snapshot_id',
-#             metavar='<string>',
-#             help='ID of snapshot')
-#         return parser
-#
-#     def take_action(self, parsed_args):
-#         client = self.app.eclsdk.conn.storage
-#         client.restore_snapshot(parsed_args.snapshot_id)
