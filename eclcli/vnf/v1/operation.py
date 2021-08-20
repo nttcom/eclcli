6f1113ce5679294946bc53508ea9b9ce541f5f87
Nishioka-Aiko <nishioka.a0@gmail.com>
2021-08-20 16:37:13 +0900

    ♻️ IF-6056 variable fixed

--- eclcli/vnf/v1/operation.py
+++ eclcli/vnf/v1/operation.py
@@ -84,9 +84,8 @@ class ShowOperation(command.ShowOne):
         data = client.get_operation(parsed_args.operation_id)
 
         if data.request_body:
-            req_body = data.request_body
             req_body_dict = {}
-            req_body_dict = req_body if type(req_body) is dict else json.loads(req_body)
+            req_body_dict = data.request_body if type(data.request_body) is dict else json.loads(data.request_body)
             setattr(data, 'request_body', json.dumps(req_body_dict, indent=2))
 
         return row_headers, (utils.get_item_properties(data, rows))
