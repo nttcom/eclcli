0cc6d5eca1146c1f9ca135365a7016920112aa50
yamashita-hiroto <hirotoy.yamashita@ntt.com>
2022-11-15 10:38:28 +0900

    :sparkles: VNF-2193 Add mlb commands

---
+++ eclcli/mlb/client.py
@@ -0,0 +1,13 @@
+import logging
+
+from eclcli.common import utils
+
+LOG = logging.getLogger(__name__)
+
+DEFAULT_API_VERSION = '1'
+API_VERSION_OPTION = 'os_mlb_api_version'
+API_NAME = "mlb"
+
+
+def build_option_parser(parser):
+    return parser
---
+++ eclcli/mlb/v1/mlb_tls_policy.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+import copy
+from eclcli.common import command, utils
+
+
+class ListTlsPolicy(command.Lister):
+
+    def get_parser(self, prog_name):
+        parser = super(ListTlsPolicy, self).get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        client = self.app.eclsdk.conn.mvna
+
+        columns = [
+            'ID',
+            'Name',
+            'Default',
+        ]
+        column_headers = copy.deepcopy(columns)
+
+        data = client.tls_policies()
+
+        return (column_headers,
+                (utils.get_item_properties(
+                    s, columns,
+                    formatters={'Metadata': utils.format_dict},
+                ) for s in data))
+
+
+class ShowTlsPolicy(command.ShowOne):
+
+    def get_parser(self, prog_name):
+        parser = super(ShowTlsPolicy, self).\
+            get_parser(prog_name)
+        parser.add_argument(
+            'tls_policy_id',
+            metavar='<tls-policy-id>',
+            help='ID of tls policy id to look up.')
+        return parser
+
+    def take_action(self, parsed_args):
+        client = self.app.eclsdk.conn.mvna
+
+        rows = [
+            'ID',
+            'Name',
+            'Default',
+            'TLS Protocols',
+            'TLS Ciphers',
+        ]
+        row_headers = rows
+
+        data = client.get_tls_policy(
+            parsed_args.tls_policy_id)
+
+        tls_protocols = data.tls_protocols
+        tls_protocols_json = json.dumps(tls_protocols, indent=2)
+        setattr(data, 'tls_protocols', tls_protocols_json)
+
+        tls_ciphers = data.tls_ciphers
+        tls_ciphers_json = json.dumps(tls_ciphers, indent=2)
+        setattr(data, 'tls_ciphers', tls_ciphers_json)
+
+        return row_headers, (utils.get_item_properties(data, rows))
